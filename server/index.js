6987a389272c95c13526bd80386eede2dcc44d0f
Dang Khoa <hnkhoa04@gmail.com>
2025-07-08 19:52:59 +0700

    create server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const upload = require("express-fileupload")
+const cors = require("cors")
+require("dotenv").config()
+const { connect } = require("mongoose")
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ extended: true }))
+app.use(cors({ credentials: true, origin: ["http://localhost:5173"] }))
+app.use(upload())
+
+app.listen(process.env.PORT, () => console.log(`Server run on port: ${process.env.PORT}`))
+
+
