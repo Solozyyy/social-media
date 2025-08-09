96640e93eb3505dc81277e7485048174acb8842e
Dang Khoa <hnkhoa04@gmail.com>
2025-08-09 21:14:58 +0700

    connect to mongo

--- server/index.js
+++ server/index.js
@@ -3,8 +3,11 @@ const upload = require("express-fileupload")
 const cors = require("cors")
 require("dotenv").config()
 const { connect } = require("mongoose")
+const dbConnect = require("./db/dbConnect")
 
 const app = express()
+
+dbConnect()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json({ extended: true }))
