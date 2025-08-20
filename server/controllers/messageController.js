f0f1ea3d6d1671182bf4991b6eeaba55283799cf
Dang Khoa <hnkhoa04@gmail.com>
2025-08-20 21:30:07 +0700

    update getMessages and getConversations

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -5,9 +5,19 @@ const HttpError = require("../models/errorModel")
 
 const createMessage = async (req, res, next) => {
     try {
+        const { receiverId } = req.params
+        const { messageBody } = req.body
 
-        res.json("create message")
+        let conversation = await ConversationModel.findOne({ participants: { $all: [req.user.id, receiverId] } })
+        //create a new conversation if it hasn't been created yet
+        if (!conversation) {
+            conversation = await ConversationModel.create({ participants: [req.user.id, receiverId], lastMessage: { text: messageBody, senderId: req.user.id } })
+        }
+        const newMessage = await MessageModel.create({ conversationId: conversation._id, senderId: req.user.id, text: messageBody })
+        //update last message in conversation
+        await conversation.updateOne({ lastMessage: { text: messageBody, senderId: req.user.id } })
 
+        res.json(newMessage)
     } catch (error) {
         return next(new HttpError(error || "error occured"))
     }
@@ -16,7 +26,14 @@ const createMessage = async (req, res, next) => {
 const getMessages = async (req, res, next) => {
     try {
 
-        res.json("get messages")
+        const { receiverId } = req.params
+        const conversation = await ConversationModel.findOne({ participants: { $all: [req.user.id, receiverId] } })
+
+        if (!conversation) {
+            return next(new HttpError("No messages with this user"))
+        }
+        const messages = await MessageModel.find({ conversationId: conversation._id }).sort({ createdAt: 1 })
+        res.json(messages)
 
     } catch (error) {
         return next(new HttpError(error || "error occured"))
@@ -26,7 +43,21 @@ const getMessages = async (req, res, next) => {
 const getConversations = async (req, res, next) => {
     try {
 
-        res.json("get conversations")
+        const conversations = await ConversationModel.find({ participants: req.user.id }).populate({
+            path: "participants", select: "fullName profilePhoto"
+        }).sort({ createdAt: -1 })
+
+        //remove current user from list
+        conversations.forEach((conversation) => {
+            conversation.participants = conversation.participants.filter(
+                (participants) => participants._id.toString() !== req.user.id.toString()
+            )
+        });
+
+        if (!conversations) {
+            return next(new HttpError("User has no conversations"))
+        }
+        res.json(conversations)
 
     } catch (error) {
         return next(new HttpError(error || "error occured"))
