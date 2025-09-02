b56c5f1c2af8cebf8854d26d17235f7e6a07ad93
Dang Khoa <hnkhoa04@gmail.com>
2025-09-02 22:41:35 +0700

    setup sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { AiOutlineHome } from 'react-icons/ai'
+import { GoMail } from 'react-icons/go'
+import { FaRegBookmark } from 'react-icons/fa'
+import { PiPaintBrushBold } from 'react-icons/pi'
 
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <menu className="sidebar">
+            <NavLink to="/" className={`sidebar__item ${({ isActive }) => isActive ? "active" : ""}`}>
+                <i className='sidebar__icon'><AiOutlineHome /></i>
+                <p>Home</p>
+            </NavLink>
+            <NavLink to="/messages" className={`sidebar__item ${({ isActive }) => isActive ? "active" : ""}`}>
+                <i className='sidebar__icon'><GoMail /></i>
+                <p>Messages</p>
+            </NavLink>
+            <NavLink to="/bookmarks" className={`sidebar__item ${({ isActive }) => isActive ? "active" : ""}`}>
+                <i className='sidebar__icon'><FaRegBookmark /></i>
+                <p>BookMarks</p>
+            </NavLink>
+            <a className={` sidebar__item ${({ isActive }) => isActive ? "active" : ""}`} >
+                <i className='sidebar__icon'><PiPaintBrushBold /></i>
+                <p>Themes</p>
+            </a>
+        </menu>
     )
 }
 
