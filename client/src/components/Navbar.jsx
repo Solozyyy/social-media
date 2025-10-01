acd64dbb06a4c464d2b735e95a9dab2c8dbb5b14
Dang Khoa <hnkhoa04@gmail.com>
2025-10-01 08:54:05 +0700

    fix profile photo in navbar not change after change avatar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { CiSearch } from 'react-icons/ci'
 import ProfileImage from './ProfileImage'
 import { useSelector } from 'react-redux'
 import { useEffect } from 'react'
+import { getUserById } from '../services'
 
 const Navbar = () => {
 
     const userId = useSelector(state => state?.user?.currentUser?.id)
-    const profilePhoto = useSelector(state => state?.user?.currentUser?.profilePhoto)
     const token = useSelector(state => state?.user?.currentUser?.token)
     const navigate = useNavigate()
-    console.log(profilePhoto);
+    //console.log(profilePhoto);
+    const [avatar, setAvatar] = useState({})
+
+    const getUser = async () => {
+        try {
+            const res = await getUserById(userId)
+            console.log(res?.data, "userData");
+            setAvatar(res?.data?.profilePhoto)
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
 
     useEffect(() => {
         if (!token) {
             navigate('/login')
+        } else {
+            getUser()
         }
     }, [])
 
@@ -37,7 +52,7 @@ const Navbar = () => {
                 </form>
                 <div className="navbar__right">
                     <Link to={`/users/${userId}`} className="navbar__proile">
-                        <ProfileImage image={profilePhoto} />
+                        <ProfileImage image={avatar} />
                     </Link>
                     {token ? <Link to="/logout" >Logout</Link> : <Link to="/login" >Login</Link>}
                 </div>
